1a705ef3ef3b3b4113bce3328bb53bfaf60fc37a
EconMaett <matthias.spichiger@bluewin.ch>
2025-10-25 12:35:06 +0200

    fixed typo

--- marronnier.R
+++ marronnier.R
@@ -3,23 +3,23 @@ library(tidyverse)
 
 ## load data ----
 # Source: https://www.swissinfo.ch/eng/swiss-oddities/spring-begins-in-geneva-when-the-horse-chestnut-tree-blossoms/88756963
-maronnier <- readr::read_csv(file = "maronnier.csv") |> 
+marronnier <- readr::read_csv(file = "marronnier.csv") |> 
   select(year, date) |> 
   mutate(date = lubridate::dmy(paste0(stringr::str_sub(date, 1, 6), year))) |> 
   mutate(first_of_jan = as.Date(paste0(year, "-01-01"))) |> 
   mutate(doy = as.numeric(date - first_of_jan)) |> 
   select(-first_of_jan)
 
-# Fix year for the extrarodinarily summer of 2003
-maronnier[maronnier$date == as.Date("2002-12-29"), ]$year <- 2003
-maronnier[maronnier$date == as.Date("2002-12-29"), ]$doy <- maronnier[maronnier$date == as.Date("2002-12-29"), ]$doy - 365
+# Fix year for the extraordinarily hot summer of 2003
+marronnier[marronnier$date == as.Date("2002-12-29"), ]$year <- 2003
+marronnier[marronnier$date == as.Date("2002-12-29"), ]$doy <- marronnier[marronnier$date == as.Date("2002-12-29"), ]$doy - 365
 
-write_csv(x = maronnier, file = "maronnier_clean.csv")
+write_csv(x = marronnier, file = "marronnier_clean.csv")
 
 ## plot ----
-maronnier_caption <- "https://ge.ch/grandconseil/secretariat/marronnier"
+marronnier_caption <- "https://ge.ch/grandconseil/secretariat/marronnier"
 
-p <- ggplot(data = maronnier, mapping = aes(x = year, y = doy)) +
+p <- ggplot(data = marronnier, mapping = aes(x = year, y = doy)) +
   geom_hline(yintercept = 0, linewidth = 0.5, colour = "darkgray") +
   geom_smooth(method = "loess", formula = "y ~ x", se = FALSE, colour = "#bc6c25") +
   geom_smooth(method = "loess", formula = "y ~ x", se = TRUE, colour = "#dda15e") +
@@ -31,38 +31,34 @@ p <- ggplot(data = maronnier, mapping = aes(x = year, y = doy)) +
     title = "Spring begins in Geneva when the official horse chestnut tree says so",
     subtitle = "Evolution of the dates of the appearance of the first official horse chestnut leaves of the year (1818-2024)",
     x = NULL, y = NULL,
-    caption = str_wrap(maronnier_caption)
+    caption = str_wrap(marronnier_caption)
   ) +
   theme_minimal(base_size = 6)
 
 print(p)
 
-ggsave(filename = "maronnier.png", plot = p, path = ".", width = 12, height = 6, units = "cm", bg = "white")
+ggsave(filename = "marronnier.png", plot = p, path = ".", width = 12, height = 6, units = "cm", bg = "white")
 
 ## combine datasets ----
 ### Geneva horse chestnut tree ----
-geneva_chestnut <- readr::read_csv("https://raw.githubusercontent.com/econmaett/marronnier/refs/heads/main/maronnier_clean.csv") |> 
+geneva_chestnut <- readr::read_csv("https://raw.githubusercontent.com/econmaett/marronnier/refs/heads/main/marronnier_clean.csv") |> 
   dplyr::mutate(date = as.Date(date), location = "Geneva")
 
-View(geneva_chestnut)
 
 ### Liestal cherry blossom ----
 liestal_cherry <- readr::read_csv("https://raw.githubusercontent.com/GMU-CherryBlossomCompetition/peak-bloom-prediction/refs/heads/main/data/liestal.csv") |> 
   dplyr::select(year, date = bloom_date, doy = bloom_doy) |> 
   dplyr::mutate(date = as.Date(date), location = "Liestal")
 
-View(liestal_cherry)
 
 ### Kyoto cherry blossom ----
 kyoto_cherry <- readr::read_csv("https://raw.githubusercontent.com/GMU-CherryBlossomCompetition/peak-bloom-prediction/refs/heads/main/data/kyoto.csv") |> 
   dplyr::select(year, date = bloom_date, doy = bloom_doy) |> 
   dplyr::mutate(date = as.Date(date), location = "Kyoto")
 
-View(kyoto_cherry)
 
 ### combine datasets ----
 phenology <- dplyr::bind_rows(geneva_chestnut, liestal_cherry, kyoto_cherry)
-View(phenology)
 
 ## plot ----
 phenology_caption <- "Vitasse, Y., Baumgarten, F., Zohner, C.M. et al. The great acceleration of plant phenological shifts. Nat. Clim. Chang. 12, 300–302 (2022). https://doi.org/10.1038/s41558-022-01283-y"
