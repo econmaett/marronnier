53e3acbce77f0442ac062b3cd6bb4126e84b3718
EconMaett <matthias.spichiger@bluewin.ch>
2025-10-26 17:23:50 +0100

    reduced alpha in geom_line by half

--- marronnier.R
+++ marronnier.R
@@ -22,7 +22,7 @@ marronnier_caption <- "https://ge.ch/grandconseil/secretariat/marronnier"
 p <- ggplot(data = marronnier, mapping = aes(x = year, y = doy)) +
   geom_hline(yintercept = 0, linewidth = 0.5, colour = "darkgray") +
   geom_smooth(method = "loess", formula = "y ~ x", se = TRUE, colour = "#954535", fill = "#954535") +
-  geom_line(linewidth = 0.5, colour = "gray") +
+  geom_line(linewidth = 0.5, colour = "gray", alpha = 0.5) +
   geom_point(size = 0.75) +
   scale_y_continuous(breaks = seq(-10, 110, 20), limits = c(-10, 110)) +
   scale_x_continuous(breaks = seq(1800, 2024, 25)) +
@@ -32,7 +32,7 @@ p <- ggplot(data = marronnier, mapping = aes(x = year, y = doy)) +
     x = NULL, y = NULL,
     caption = str_wrap(marronnier_caption)
   ) +
-  theme_bw(base_size = 10) +
+  theme_minimal(base_size = 8) +
   theme(panel.grid.minor = element_blank())
 
 print(p)
@@ -54,7 +54,6 @@ kyoto_cherry <- readr::read_csv("https://raw.githubusercontent.com/GMU-CherryBlo
   dplyr::select(year, date = bloom_date, doy = bloom_doy) |> 
   dplyr::mutate(date = as.Date(date), location = "Kyoto")
 
-
 ### combine datasets ----
 phenology <- dplyr::bind_rows(geneva_chestnut, liestal_cherry, kyoto_cherry)
 
@@ -62,9 +61,8 @@ phenology <- dplyr::bind_rows(geneva_chestnut, liestal_cherry, kyoto_cherry)
 phenology_caption <- "Vitasse, Y., Baumgarten, F., Zohner, C.M. et al. The great acceleration of plant phenological shifts. Nat. Clim. Chang. 12, 300–302 (2022). https://doi.org/10.1038/s41558-022-01283-y"
 
 p <- ggplot(data = phenology |> filter(year >= 1900), mapping = aes(x = year, y = doy)) +
-  geom_smooth(method = "loess", formula = "y ~ x", se = FALSE) +
   geom_smooth(method = "loess", formula = "y ~ x", se = TRUE) +
-  geom_line(linewidth = 0.5) +
+  geom_line(linewidth = 0.5, alpha = 0.5) +
   geom_point(size = 1) +
   facet_grid(rows = vars(location), scale = "free_y") +
   theme_minimal() +
